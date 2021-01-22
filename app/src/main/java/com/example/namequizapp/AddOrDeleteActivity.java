feac4d79883d1828f32ea6a38e39e975cf291388
Jon Vollset <42602724+574905@users.noreply.github.com>
2021-01-22 13:09:16 +0100

    Added new activity

---
+++ app/src/main/java/com/example/namequizapp/AddOrDeleteActivity.java
@@ -0,0 +1,14 @@
+package com.example.namequizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddOrDeleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_or_delete);
+    }
+}
