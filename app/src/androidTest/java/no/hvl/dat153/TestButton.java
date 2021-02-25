0cd4f9cfd8204911ddea581580e6bbb20ebeaacb
Jon Vollset <42602724+574905@users.noreply.github.com>
2021-02-25 11:40:34 +0100

    Added a Testcase!

---
+++ app/src/androidTest/java/no/hvl/dat153/TestButton.java
@@ -0,0 +1,37 @@
+package no.hvl.dat153;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.namequizapp.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import no.hvl.dat153.activities.MainActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSubstring;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class TestButton {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testDatabaseButton(){
+        onView(withId(R.id.db_button1)).perform(ViewActions.click());
+        onView(withId(R.id.button_add)).check(matches(withSubstring("Add new person")));
+
+    }
+
+
+}
